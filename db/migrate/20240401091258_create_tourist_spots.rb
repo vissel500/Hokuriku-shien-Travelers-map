420162f2afde1cdb95a226ca997ae50166fa583d
岸本晃洋 <koyo500@MacBook-Air.local>
2024-04-01 22:36:49 +0900

    データベース導入

---
+++ db/migrate/20240401091258_create_tourist_spots.rb
@@ -0,0 +1,12 @@
+class CreateTouristSpots < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tourist_spots do |t|
+      t.string :name
+      t.string :category
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
