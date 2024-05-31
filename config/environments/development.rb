820ca1731459aa1365069e76ec6f1a662b931099
岸本晃洋 <koyo500@MacBook-Air.local>
2024-05-31 16:57:39 +0900

    お問い合わせ機能の本番環境への適用

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "https://hokuriku-shien-travelers-map-c9f671e3c233.herokuapp.com/", protocol: "https" }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
